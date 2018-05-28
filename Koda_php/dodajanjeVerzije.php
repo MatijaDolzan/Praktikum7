b7c3b23794ac77078782290328e4f431041cc64e
Matija Dolžan <matija.dolzan@gmail.com>
2018-05-28 17:16:47 +0200

    Brez absolutnih poti
    da

--- Koda_php/dodajanjeVerzije.php
+++ Koda_php/dodajanjeVerzije.php
@@ -5,8 +5,8 @@ $uporabnik=null;
 //$uporabnik=$_SESSION['current_user'];
 $_SESSION['current_user']=1;
 
-include   'C:\wamp64\www\Praktikum\razredi\Verzija.php';
-include_once   'C:\wamp64\www\Praktikum\razredi\Iprivolitev.php';
+require   'razredi\Verzija.php';
+require_once  'razredi\Iprivolitev.php';
 ?>
 <h2>Dodajanje nove verzije</h2>
 <form action="workerji/dodajanjeVerzije_worker.php" method="post">
