071e25f050ff0e5e20e964f9937c8439f52eeda9
Petra <petra.bencek@gmail.com>
2018-06-04 19:06:18 +0200

    Zdaj pa dela odjava pravilno
    Odjavi prav iz google-a + odjavi iz nase strani, in tako se lahko nekdo drug prijavi

--- Google_prijava/logout_worker.php
+++ Google_prijava/logout_worker.php
@@ -4,7 +4,7 @@ session_start();
 
 if(session_destroy()) {
     
-    header("Location: index.php");
+    header("Location: https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost/Praktikum/index.php");
     exit();
     
 } 
