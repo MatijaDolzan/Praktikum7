520c43610927c4e4301c52ec5b23db79e7e9e367
BrodMatic <matic.brod@student.um.si>
2018-06-05 22:55:38 +0200

    Razred Podpisnik & Prilagoditev logout za Google

--- Koda_php/logout_worker.php
+++ Koda_php/logout_worker.php
@@ -2,10 +2,18 @@
 
 session_start();
 
-if(session_destroy()) {
-    
-    header("Location: index.php");
+if(isset($_SESSION['google_id'])) {
+
+    session_destroy();
+    header("Location: https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://localhost/Praktikum/index.php");
     exit();
     
+}else{
+
+    session_destroy();
+    header("Location: index.php");
+    exit();
+
 }
+
 ?>
