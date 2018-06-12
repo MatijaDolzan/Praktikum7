7de4b181abf1f989ebfbaf4df704fbd2d50a885f
BrodMatic <matic.brod@student.um.si>
2018-06-12 21:03:48 +0200

    Update MainCode Part 3 - MB-5

---
+++ Koda_php/menu.php
@@ -0,0 +1,88 @@
+<?php
+session_start();
+?>
+
+<?php ini_set('default_charset', 'UTF-8');
+header('Content-type', 'text/html; charset=UTF-8');
+?>
+
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #778899;
+    float:center;
+}
+
+h1 {
+    align:center;
+    color:#808080;
+}
+
+li {
+    display: inline-block;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #708090;
+}
+
+</style>
+
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+
+<center><h1><i><a class="active" href="index.php">Podjetje d.o.o.</a></i></h1></center>
+
+<p><div><ul>
+ <center>
+  <div id="logout_div"><li>
+  <a href="dodajanjePrivolitve.php">Dodaj privolitev</a></li>
+  <li><a href="list.php">Seznam privolitev</a></li>
+  <li><a href="iskanje.php">Iskanje privolitev</a></li>
+  <li><a href="seznam_upravljavcev.php">Upravljalci</a></li>  
+  <li><a href="splosni_pogoji.php">Splosni pogoji</a></li>
+  <li><a href="logout_worker.php">Odjava</a></li></div>
+  <div id="login_register_div"><li><a href="login.php" class='login'>Prijava</a></li>
+  <li><a href="register.php">Registracija</a></li></div></center>  
+</ul></div></p>
+
+
+
+
+<?php
+if(isset($_SESSION['current_user'])){
+?>
+<script type="text/javascript">document.getElementById('login_register_div').style.display = 'none'; document.getElementById('login_register_div').style.maxHeight = "0px" ; document.getElementById('logout_div').style.display = 'block';</script>
+<?php 
+}else{
+?>
+<script type="text/javascript">document.getElementById('login_register_div').style.display = 'block'; document.getElementById('logout_div').style.display = 'none'; document.getElementById('logout_div').style.maxHeight = "0px"</script>
+<?php 
+}
+?>
--- Koda_php/podpisovanje.php
+++ Koda_php/podpisovanje.php
@@ -1,52 +1,107 @@
 <?php
-
-session_start();
-if(isset($_SESSION['sign_error'])){
-    if($_SESSION['returnurl'] === FALSE){
-    }else{
-        $returnurl = $_SESSION['returnurl'] . "?success=false";
-        ?>
-<form action="<?php echo $returnurl;?>" method="post">
-<input type='submit' name='submit' value='Vrni se' class='login' />
-</form>
-  <?php 
-    }
-    ?>
-<script>alert('<?php echo $_SESSION['sign_error'];?>');</script>
-<?php 
- unset($_SESSION['sign_error']);
-}else{
-
 require    'razredi\Privolitev.php';
 require    'razredi\Verzija.php';
 require    'razredi\Pooblascenec.php';
 require    'razredi\Upravljalec.php';
 require    'razredi\Checkboxi.php';
-if (isset($_GET['privolitev']) && isset($_GET['verzija']) && isset($_GET['email'])){
-    if(isset($_SERVER['HTTP_REFERER'])){
-        $_SESSION['returnurl'] = $_SERVER['HTTP_REFERER'];
+require 'menu.php';
+
+//PREVERJANJE ALI JE PRI HRAMBI PODPISNIKA PRISLO DO NAPAKE
+if(isset($_SESSION['sign_error'])){
+    
+    //V PRIMERU DA SE JE ZGODIL ERROR - PREVERIMO ALI IMAMO RETURN-URL
+    if($_SESSION['returnurl'] === FALSE){
+        
+        //V PRIMERU DA NIMAMO RETURN-URL - IZPISEMO NAPAKO
+        echo $_SESSION['sign_error'];
+        
     }else{
-        $_SESSION['returnurl'] = FALSE;
+        
+        //V PRIMERU DA IMAMO RETURN-URL - IZPISEMO NAPAKO IN RETURN-URL Z OBVESTILOM O NEUSPESNEM POSTOPKU
+        $returnurl = $_SESSION['returnurl'] . "?success=false";
+        echo $_SESSION['sign_error'];
+        ?>
+		<form action="<?php echo $returnurl;?>" method="post">
+		<input type='submit' name='submit' value='Vrni se' class='login' />
+		</form>
+		<?php 
+		
     }
-    $privolitev=new Privolitev("");
-    $privolitev=$privolitev->getIzBazeId($_GET['privolitev']);
+    session_destroy();
+    
+ //PREVERJANJE ALI JE BILA HRAMBA USPESNA
+}elseif(isset($_SESSION['returnprocess'])){
+    
+    //V PRIMERU DA JE BILA - PREVERIMO ALI IMAMO RETURN-URL
+    if($_SESSION['returnurl'] === FALSE){
+        
+        //V PRIMERU DA NIMAMO RETURN-URL - IZPISEMO USPEH IN MOZNOST EXPORTA V PDF
+        echo "Privolitev shranjena!";
+        ?>
+        <form action="export.php" method="post">
+		<input type="hidden" name="export_id_verz" value="<?php echo $_SESSION['verzija'];?>">
+		<input type="submit" value="Izvozi v PDF">
+		</form>
+        <?php
+        
+    }else{
+        
+        //V PRIMERU DA IMAMO RETURN-URL - IZPISEMO USPEH, RETURN-URL Z OBVESTILOM O USPESNEM POSTOPKU IN MOZNOST EXPORTA V PDF
+        $returnurl = $_SESSION['returnurl'] . "?success=true";
+        echo "Privolitev shranjena!";
+        ?>
+    	<form action="export.php" method="post">
+		<input type="hidden" name="export_id_verz" value="<?php echo $_SESSION['verzija'];?>">
+		<input type="submit" value="Izvozi v PDF">
+		</form>
+    	<form action="<?php echo $returnurl;?>" method="post">
+    	<input type='submit' name='submit' value='Vrni se' class='login' />
+    	</form>
+    	<?php 
+    	
+    }
+    session_destroy();
+    
+//KODA ZA DINAMICNI PRIKAZ PRIVOLITVE
+}else{
+
+//PREVERJANJE ALI JE PODANA ZAHTEVA USTREZNA
+if (isset($_GET['verzija']) && isset($_GET['email'])){
+    
+    //PREVERJANJE ZA RETURN-URL
+    if(isset($_SERVER['HTTP_REFERER'])){
+        
+        $_SESSION['returnurl'] = $_SERVER['HTTP_REFERER'];
+        
+    }else{
+        
+        $_SESSION['returnurl'] = FALSE;
+        
+    }
+    
     $verzija=new Verzija("","");
     $verzija=$verzija->getIzBazeV($_GET['verzija']);
+    $privolitev_id = $verzija->getFK_ver_priv();
+    $privolitev=new Privolitev("");
+    $privolitev=$privolitev->getIzBazeId($privolitev_id);
     $upravljalec= new Upravljalec("", "", "", "");
-    $upravljalec=$upravljalec->getIzBaze($_GET['privolitev']);
+    $upravljalec=$upravljalec->getIzBaze($privolitev_id);
     $arr=array(10);
     $ch=new Checkbox(null, "","");
     $array=$ch->getVseCheckboxe($_GET['verzija']);
-    for ($j=0;$j<10;$j++){
-        if (isset($array[$j])){
+    
+    for($j=0;$j<10;$j++){
+        
+        if(isset($array[$j])){
+            
             $ch=$array[$j];
             $arr[$j]=$ch;
-        }
-        else{
+            
+        }else{
+            
             $arr[$j]=new Checkbox(null, "", "");
+            
         }
-        
-        
     }
     
     $chbx1=$arr[0];
@@ -60,110 +115,153 @@ if (isset($_GET['privolitev']) && isset($_GET['verzija']) && isset($_GET['email'
     $chbx9=$arr[8];
     $chbx10=$arr[9];
     $pooblascenec=new Pooblascenec("", "", "");
+    
     if ($verzija->getPoob()!=0){
-    $pooblascenec=$pooblascenec->getIzBazePoob($verzija->getPoob());
-    }
-
-$stevec=0;
-for ($i = 0; $i < 10; $i++) {
-    if ($arr[$i]->getCheckbox()!=""){
-        $stevec++;
+        
+        $pooblascenec=$pooblascenec->getIzBazePoob($verzija->getPoob());
         
     }
-    
-}
+
+    $stevec=0;
+    for ($i = 0; $i < 10; $i++) {
+        
+        if ($arr[$i]->getCheckbox()!=""){
+            $stevec++;
+            
+        }
+    }
 ?>
-<form method="post" action="podpisovanje_worker.php">
-<table>
-naslov:<?php echo $privolitev->getNaslov() ?>
-text: <?php echo $verzija->getText() ?>
-hramba:<?php echo $verzija->getHramba() ?>
-<?php 
+
+	<form method="post" action="podpisovanje_worker.php">
+	<table>
+	Naslov: <?php echo $privolitev->getNaslov() ?><br>
+	Text: <?php echo $verzija->getText() ?><br>
+	Hramba:<?php echo $verzija->getHramba() ?><br>
+
+<?php
 if ($stevec!=0){
-echo 'Oznacite checkboxe:';
+    echo 'Oznacite checkboxe:<br>';
 }
 
 if ($chbx1->getCheckbox()!="") {
- echo $chbx1->getCheckbox();?>
+    
+    echo $chbx1->getCheckbox();
+    ?>
     <input type="checkbox" name="chxb1" value="<?php echo $chbx1->getCheckbox()?>"/>
     <input type="hidden" name="chxb1_id" value="<?php echo $chbx1->getId()?>"/>
-<?php 
+    <br>
+	<?php 
+	
 }
 
 if ($chbx2->getCheckbox()!="") {
- echo $chbx2->getCheckbox();?>
+    
+    echo $chbx2->getCheckbox();?>
     <input type="checkbox" name="chxb2" value="<?php echo $chbx2->getCheckbox()?>"/>
     <input type="hidden" name="chxb2_id" value="<?php echo $chbx2->getId()?>"/>
-<?php 
+    <br>
+	<?php 
+	
 }
 
 if ($chbx3->getCheckbox()!="") {
-echo $chbx3->getCheckbox();?>
+    
+    echo $chbx3->getCheckbox();?>
     <input type="checkbox" name="chxb3" value="<?php echo $chbx3->getCheckbox()?>"/>
     <input type="hidden" name="chxb3_id" value="<?php echo $chbx3->getId()?>"/>
-<?php
+    <br>
+	<?php
+
 }
 
 if ($chbx4->getCheckbox()!="") {
-echo $chbx4->getCheckbox();?>
+    
+    echo $chbx4->getCheckbox();?>
     <input type="checkbox" name="chxb4" value="<?php echo $chbx4->getCheckbox()?>"/>
     <input type="hidden" name="chxb4_id" value="<?php echo $chbx4->getId()?>"/>
-<?php 
+    <br>
+	<?php 
+
 }
 
 if ($chbx5->getCheckbox()!="") {
-echo $chbx5->getCheckbox();?>
+    
+    echo $chbx5->getCheckbox();?>
     <input type="checkbox" name="chxb5" value="<?php echo $chbx5->getCheckbox()?>"/>
     <input type="hidden" name="chxb5_id" value="<?php echo $chbx5->getId()?>"/>
-<?php 
+    <br>
+	<?php 
+
 }
 
 if ($chbx6->getCheckbox()!="") {
-echo $chbx6->getCheckbox();?>
+    
+    echo $chbx6->getCheckbox();?>
     <input type="checkbox" name="chxb6" value="<?php echo $chbx6->getCheckbox()?>"/>
     <input type="hidden" name="chxb6_id" value="<?php echo $chbx6->getId()?>"/>
-<?php 
+    <br>
+	<?php 
+
 }
 
 if ($chbx7->getCheckbox()!="") {
-echo $chbx7->getCheckbox();?>
+    
+    echo $chbx7->getCheckbox();?>
     <input type="checkbox" name="chxb7" value="<?php echo $chbx7->getCheckbox()?>"/>
     <input type="hidden" name="chxb7_id" value="<?php echo $chbx7->getId()?>"/>
-<?php 
+    <br>
+	<?php 
+
 }
 
 if ($chbx8->getCheckbox()!="") {
-echo $chbx8->getCheckbox();?>
+    
+    echo $chbx8->getCheckbox();?>
     <input type="checkbox" name="chxb8" value="<?php echo $chbx8->getCheckbox()?>"/>
     <input type="hidden" name="chxb8_id" value="<?php echo $chbx8->getId()?>"/>
-<?php 
+    <br>
+	<?php 
+
 }
 
 if ($chbx9->getCheckbox()!="") {
-echo $chbx9->getCheckbox();?>
+    
+    echo $chbx9->getCheckbox();?>
     <input type="checkbox" name="chxb9" value="<?php echo $chbx9->getCheckbox()?>"/>
     <input type="hidden" name="chxb9_id" value="<?php echo $chbx9->getId()?>"/>
-<?php 
+    <br>
+	<?php 
+
 }
 
 if ($chbx10->getCheckbox()!="") {
-echo $chbx10->getCheckbox();?>
+    
+    echo $chbx10->getCheckbox();?>
     <input type="checkbox" name="chxb10" value="<?php echo $chbx10->getCheckbox()?>"/>
     <input type="hidden" name="chxb10_id" value="<?php echo $chbx10->getId()?>"/>
-<?php 
-}
-?>
+    <br>
+	<?php 
 
-upravljalec:<?php echo $upravljalec->getName() +', '+$upravljalec->getSubname()+', '+$upravljalec->getAddress()?>
-<?php if ($pooblascenec->getIme()!="" && $pooblascenec->getPriimek()!="" && $pooblascenec->getNaslov()!=""){?>
-Pooblascenec:<?php echo $pooblascenec->getIme() +', '+$pooblascenec->getPriimek()+', '+$pooblascenec->getNaslov()?>
-<?php 
+}
+
+echo "Upravljalec: <br>" . $upravljalec->getName() . ' ' . $upravljalec->getSubname() . ',  ' . $upravljalec->getAddress() . "<br>";
+
+if ($pooblascenec->getIme()!="" && $pooblascenec->getPriimek()!="" && $pooblascenec->getNaslov()!=""){
+    
+    echo "Pooblascenec: <br>" . $pooblascenec->getIme() . ' ' . $pooblascenec->getPriimek() . ',  ' . $pooblascenec->getNaslov() . "<br>"; 
+	
 }
 ?>
+<br>
 <input type="hidden" name="email" value="<?php echo $_GET['email'];?>">
 <input type="hidden" name="verzija" value="<?php echo $_GET['verzija'];?>">
 <input type="submit" action="" name="" value="Podpisujem privolitev">
 </table>
+</form>
+
+<!-- REDIRECT ZA PODPIS MLADOLETNIH OSEB -->
+<form method="post" action ="podpisovanje_mladoletnih.php">
+<input type="submit" value="Star sem manj kot 16 let">
 </form>
 
 <?php 
